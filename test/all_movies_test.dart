8d5ba8a66cfc3c8873f90d8bf4eb7fd58ec5ce83
Bruno <brunomilliet89@gmail.com>
2019-07-07 11:50:07 -0300

    fromJson test

---
+++ test/all_movies_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movie_app/models/all_movies.dart';
+import 'package:movie_app/models/movie.dart';
+
+import 'json_mock.dart';
+
+void main() {
+  final _json = JsonMock.allMoviesJson;
+  AllMovies _all_movies;
+
+  test('Should return instance of allMovies from correct json', () {
+    _all_movies = new AllMovies.fromJson(_json);
+    List<Movie> _movies = _all_movies.movies;
+    int _movies_count = _all_movies.movies.length;
+
+    expect(_all_movies is AllMovies, true);
+    expect(_movies is List<Movie>, true);
+    expect(_movies_count, 2);
+  });
+}
--- test/movie_test.dart
+++ test/movie_test.dart
@@ -4,20 +4,19 @@ import 'package:movie_app/models/movie.dart';
 import 'json_mock.dart';
 
 void main() {
-  final _correct_json = JsonMock.moviesJson;
-  Movie _movies;
+  final _json = JsonMock.moviesJson;
+  Movie _movie;
 
-  test('Should return build movie from correct json', () {
-    _movies = new Movie.fromJson(_correct_json);
+  test('Should return instance of movie from correct json', () {
+    _movie = new Movie.fromJson(_json);
 
-    var _isMovieType = true;
-    var _id = _movies.id;
-    var _title = _movies.title;
-    var _overview = _movies.overview;
-    var _poster_path = _movies.poster_path;
-    var _vote_average = _movies.vote_average;
+    var _id = _movie.id;
+    var _title = _movie.title;
+    var _overview = _movie.overview;
+    var _poster_path = _movie.poster_path;
+    var _vote_average = _movie.vote_average;
 
-    expect(_isMovieType, _movies is Movie);
+    expect(_movie is Movie, true);
     expect(_id, 1234);
     expect(_title, 'mock title');
     expect(_overview, 'mock overview');
